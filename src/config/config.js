a8c3b7cfe215419ca102ca2cfd46c0f7f64c0e45
MOZZ\admin <yeong0319@naver.com>
2023-08-09 18:30:28 +0900

    config : RDS서버에  mySQL연결

---
+++ src/config/config.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require("sequelize");
+
+require("dotenv").config();
+
+const development = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+};
+const test = {
+  username: "root",
+  password: null,
+  database: "database_test",
+  host: "127.0.0.1",
+  dialect: "mysql",
+};
+const production = {
+  username: "root",
+  password: null,
+  database: "database_production",
+  host: "127.0.0.1",
+  dialect: "mysql",
+};
+
+
+module.exports = { development, production, test };
