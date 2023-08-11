ca4801ef69bbe80b8931ca771f64dd98dff31d88
MOZZ\admin <yeong0319@naver.com>
2023-08-11 22:28:50 +0900

    fix : controller연결 안된거 연결

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
+const UserController = require("./user.controllers")
+const userController = new UserController()
+
 router.post("/signup")
 
 module.exports = router
